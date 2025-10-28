48dc61d63ed2cf2a659e054d6b87769ad5a98bb8
Salvatore Mandra <smandra@google.com>
2025-10-28 09:38:47 -0700

    Fix `tnco.Parallel` when `joblib` is not installed.

--- tnco/parallel.py
+++ tnco/parallel.py
@@ -317,9 +317,12 @@ def Parallel(core: Callable,
         else:
             results = list(
                 map(
-                    lambda idx, xs: core_(
-                        *xs[:-1], idx=idx, status=status, **xs[-1], **buffers),
-                    range(n_runs), args))
+                    lambda idx, xs: core_(*xs[:-1],
+                                          idx=idx,
+                                          status=status,
+                                          stop=stop,
+                                          **xs[-1],
+                                          **buffers), range(n_runs), args))
 
         # Close progressbar
         if verbose >= 2:
