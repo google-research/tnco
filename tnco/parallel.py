de42cbb5118dbac4d482cf390b5a9c5ce1c072d6
Salvatore Mandra <smandra@google.com>
2025-10-28 17:32:19 -0700

    Use `map` if `n_jobs=1`

--- tnco/parallel.py
+++ tnco/parallel.py
@@ -168,12 +168,15 @@ def Parallel(core: Callable,
         >  'stopped']
     """
     # Try to load joblib
-    try:
-        from joblib import Parallel, delayed, parallel_config
-        use_joblib = True
-    except ImportError:
-        warn("Cannot load 'joblib'. Falling to sequential.")
+    if n_jobs == 1:
         use_joblib = False
+    else:
+        try:
+            from joblib import Parallel, delayed, parallel_config
+            use_joblib = True
+        except ImportError:
+            warn("Cannot load 'joblib'. Falling to sequential.")
+            use_joblib = False
 
     # If code is run from IPython, we can use a larger number of refresh per
     # second. Otherwise, let's use a smaller number of refresh per seconds to
